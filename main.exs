fc8f4c43b35f2db53610810ca2f77948fedd21ac
jagestah <mckeeverjohnj@gmail.com>
2020-01-02 19:19:27 -0700

    updated with input from my homie Gaia

--- main.exs
+++ main.exs
@@ -1,11 +1,10 @@
-last_coin_face = "+"
 input1 = "-+-++++++++++-++++++++++"
 input2 = "+-+"
 input3 = "-+-+-"
 
 defmodule Recursion do
-  def count(input, last_coin_face) do
-    do_count(String.graphemes(input), last_coin_face, 0)
+  def count(input) do
+    do_count(String.graphemes(input), "+", 0)
   end
 
   defp do_count([], _, flip_count) do
@@ -13,15 +12,14 @@ defmodule Recursion do
   end
 
   defp do_count([head | tail], last_coin_face, flip_count ) when head == last_coin_face do
-    do_count(tail, last_coin_face, flip_count)
+    do_count(tail, head, flip_count)
   end
 
   defp do_count([head | tail], last_coin_face, flip_count ) when head != last_coin_face do
-    last_coin_face = head
-    do_count(tail, last_coin_face, flip_count + 1)
+    do_count(tail, head, flip_count + 1)
   end
 end
 
-Recursion.count(input1, last_coin_face)
-Recursion.count(input2, last_coin_face)
-Recursion.count(input3, last_coin_face)
+Recursion.count(input1)
+Recursion.count(input2)
+Recursion.count(input3)
